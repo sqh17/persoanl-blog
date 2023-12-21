41bdd4077ceb1cacc0070408bba3054a029962ca
sqh17 <852104785@qq.com>
2023-12-22 00:08:31 +0800

    feat：添加环境配置，二级目录下导致的路由丢失问题处理

--- blog-front/vite.config.ts
+++ blog-front/vite.config.ts
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import Components from 'unplugin-vue-components/vite'
@@ -9,29 +9,36 @@ import { ElementPlusResolver, AntDesignVueResolver } from 'unplugin-vue-componen
 import { resolve } from 'path'
 import eslintPlugin from 'vite-plugin-eslint'
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    Components({
-      resolvers: [
-        ElementPlusResolver(),
-        AntDesignVueResolver({
-          importStyle: false // css in js
-        })
-      ]
-    }),
-    AutoImport({
-      resolvers: [ElementPlusResolver()]
-    }),
-    // 配置vite在运行的时候自动检测eslint规范
-    eslintPlugin({
-      include: ['src/**/*.ts', 'src/**/*.js', 'src/**/*.vue', 'src/*.ts', 'src/*.js', 'src/*.vue']
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src') // 设置 `@` 指向 `src` 目录
+export default defineConfig(({ mode, command, ssrBuild }) => {
+  const root = process.cwd()
+  const env = loadEnv(mode, root)
+  console.log(env)
+  return {
+    // base: './', // 设置打包路径
+    base: process.env.VITE_BASE_PATH,
+    plugins: [
+      vue(),
+      vueJsx(),
+      Components({
+        resolvers: [
+          ElementPlusResolver(),
+          AntDesignVueResolver({
+            importStyle: false // css in js
+          })
+        ]
+      }),
+      AutoImport({
+        resolvers: [ElementPlusResolver()]
+      }),
+      // 配置vite在运行的时候自动检测eslint规范
+      eslintPlugin({
+        include: ['src/**/*.ts', 'src/**/*.js', 'src/**/*.vue', 'src/*.ts', 'src/*.js', 'src/*.vue']
+      })
+    ],
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src') // 设置 `@` 指向 `src` 目录
+      }
     }
   }
 })
